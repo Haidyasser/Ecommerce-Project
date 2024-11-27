dc8712d113c599d7d98d501a075de90bdc975e12
Haidyasser <haidyasser3@gmail.com>
2024-11-27 20:59:09 +0200

    add user info to the request object

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,17 +21,19 @@ exports.register = async (req, res) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.FAIL, message: errors.array() });
+            return res.status(httpStatus.BAD_REQUEST)
+            .json({ status: httpStatus.FAIL, message: errors.array() });
         }
         const user = new User(req.body);
         const oldUser = await User.findOne({ email: user.email });
         if (oldUser) {
-            return res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.FAIL, message: "User already exists" });
+            return res.status(httpStatus.BAD_REQUEST)
+            .json({ status: httpStatus.FAIL, message: "User already exists" });
         }
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(user.password, salt);
-        const token = await generateToken({ email: user.email , id: user._id , role: user.role });
-        user.token = token;
+        // const token = await generateToken({ email: user.email , id: user._id , role: user.role });
+        // user.token = token;
         await user.save();
         res.status(httpStatus.CREATED).json({ status: httpStatus.SUCCESS, data: user.token });
     }
@@ -44,17 +46,20 @@ exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
-            return res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.FAIL, message: "Email and password are required" });
+            return res.status(httpStatus.BAD_REQUEST)
+            .json({ status: httpStatus.FAIL, message: "Email and password are required" });
         }
 
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.FAIL, message: "User not found" });
+            return res.status(httpStatus.BAD_REQUEST)
+            .json({ status: httpStatus.FAIL, message: "User not found" });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.FAIL, message: "Invalid credentials" });
+            return res.status(httpStatus.BAD_REQUEST)
+            .json({ status: httpStatus.FAIL, message: "Invalid credentials" });
         }
 
         const token = await generateToken({ email: user.email, id: user._id , role: user.role });
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -4,13 +4,13 @@ const httpStatus = require('../utilities/httpStatus');
 exports.verifyToken = async (req, res, next) => {
     try {
         const authHeader = req.headers['authorization'];
-        if (!authHeader) {
-            return res.status(httpStatus.UNAUTHORIZED).json({ status: httpStatus.FAIL, message: 'Access denied' });
-        }
         const token = authHeader && authHeader.split(' ')[1];
+        if (!token) {
+            return res.status(httpStatus.UNAUTHORIZED).json({ status: httpStatus.FAIL, message: "Access denied" });
+        }
 
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        // req.user = decoded;
+        req.user = decoded;
         next();
     }
     catch (error) {
