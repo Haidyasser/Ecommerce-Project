7d340d5bc1f1080d1c057248b7631f6f9ff5b7d4
Haidyasser <haidyasser3@gmail.com>
2024-11-28 12:57:01 +0200

    add middlewares to product routes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/productController');
 const {productValidation} = require('../middlewares/validationSchema');
+const {verifyTokenAdmin, verifyToken, verifyTokenAndAuthorized} = require('../middlewares/verifyToken');
 
 const router = express.Router();
 
 router.route('/')
-    .get(getProducts)
-    .post(productValidation(), createProduct);
+    .get(verifyToken, getProducts)
+    .post(verifyTokenAdmin, productValidation(), createProduct);
 
 router.route('/:id')
-    .get(getProduct)
-    .patch(updateProduct)
-    .delete(deleteProduct)
+    .get(verifyToken, getProduct)
+    .patch(verifyTokenAdmin, updateProduct)
+    .delete(verifyTokenAdmin, deleteProduct);
 
 module.exports = router;
