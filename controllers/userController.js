767ffe0bde6cd41f14de589ac3a712e3498c6028
Haidyasser <haidyasser3@gmail.com>
2024-11-28 11:51:00 +0200

    handle user crud operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel");
-const httpStatus = require("../utilities/httpStatus");
+const httpStatus = require("../utils/httpStatus");
 const bcrypt = require("bcrypt");
-const generateToken = require("../utilities/generateToken");
+const generateToken = require("../utils/generateToken");
 const { validationResult } = require("express-validator");
 
 exports.getUsers = async (req, res) => {
@@ -17,62 +17,41 @@ exports.getUsers = async (req, res) => {
     }
 };
 
-exports.register = async (req, res) => {
+exports.getUser = async (req, res) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(httpStatus.BAD_REQUEST)
-            .json({ status: httpStatus.FAIL, message: errors.array() });
-        }
-        const user = new User(req.body);
-        const oldUser = await User.findOne({ email: user.email });
-        if (oldUser) {
-            return res.status(httpStatus.BAD_REQUEST)
-            .json({ status: httpStatus.FAIL, message: "User already exists" });
-        }
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-        // const token = await generateToken({ email: user.email , id: user._id , role: user.role });
-        // user.token = token;
-        await user.save();
-        res.status(httpStatus.CREATED).json({ status: httpStatus.SUCCESS, data: user });
-    }
-    catch (error) {
-        res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.ERROR, message: error.message });
-    }
-};
-
-exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(httpStatus.BAD_REQUEST)
-            .json({ status: httpStatus.FAIL, message: "Email and password are required" });
-        }
-
-        const user = await User.findOne({ email });
+        const user = await User.findById(req.params.id, { "__v": false });
         if (!user) {
-            return res.status(httpStatus.BAD_REQUEST)
-            .json({ status: httpStatus.FAIL, message: "User not found" });
+            return res.status(httpStatus.NOT_FOUND).json({ status: httpStatus.FAIL, message: "User not found" });
         }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(httpStatus.BAD_REQUEST)
-            .json({ status: httpStatus.FAIL, message: "Invalid credentials" });
+        res.status(httpStatus.OK).json({ status: httpStatus.SUCCESS, data: user });
+    }
+    catch (error) {
+        res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.ERROR, message: error.message });
+    }
+};
+exports.updateUser = async (req, res) => {
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updateUser) {
+            return res.status(httpStatus.NOT_FOUND).json({ status: httpStatus.FAIL, message: "User not found" });
         }
-
-        const token = await generateToken({ email: user.email, id: user._id , role: user.role });
-        user.token = token;
-        await user.save();
-
-        res.status(httpStatus.OK).json({ status: httpStatus.SUCCESS, data: user.token});
+        res.status(httpStatus.OK).json({ status: httpStatus.SUCCESS, data: updateUser });
     }
     catch (error) {
         res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.ERROR, message: error.message });
     }
 };
 
-exports.updateUser = async (req, res) => {};
-exports.deleteUser = async (req, res) => {};
-exports.getUser = async (req, res) => {};
+exports.deleteUser = async (req, res) => {
+    try {
+        const deleteUser = await User.findByIdAndDelete(req.params.id);
+        if (!deleteUser) {
+            return res.status(httpStatus.NOT_FOUND).json({ status: httpStatus.FAIL, message: "User not found" });
+        }
+        res.status(httpStatus.OK).json({ status: httpStatus.SUCCESS, message: "User deleted successfully" });
+    }
+    catch (error) {
+        res.status(httpStatus.BAD_REQUEST).json({ status: httpStatus.ERROR, message: error.message });
+    }
+};
+
