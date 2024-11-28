d1d7ef55c42071647b1998bac1df0055ecdb7972
Haidyasser <haidyasser3@gmail.com>
2024-11-28 11:48:35 +0200

    update utils dir name

---
+++ utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (payload) => {
+    return jwt.sign(
+        payload, 
+        process.env.SECRET_KEY,
+        {expiresIn: '5d'}
+    );
+}
